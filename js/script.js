a4e8275cb81fe891de57d95be5f2b96cd4df09a9
Yaa Otchere <yaa@yaaotchere.ca>
2020-05-18 16:50:12 -0400

    working on one page portfolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 function displayNav() {
     var visibleNav = document.getElementById('navigation').classList.toggle("display-nav");
+    //make it so the menu disappears after I click on a link on the nav
 }
